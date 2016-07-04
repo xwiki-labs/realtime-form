c5d76a73ff3505028b7e440c4d66dc20167647fb
yflory <yann.flory@xwiki.com>
2016-07-04 17:08:57 +0200

    Update version to 1.4

--- src/xwiki/this.js
+++ src/xwiki/this.js
@@ -2,5 +2,5 @@ Package(function (pack, XWiki) {
     pack.setName("Realtime Form Editor");
     pack.setDescription("Collaboratively edit your XWiki forms with others");
     pack.setExtensionId("org.xwiki.contrib:xwiki-contrib-rtwiki");
-    pack.setXarName("realtime-form-1.3.xar")
+    pack.setXarName("realtime-form-1.4.xar")
 });
